95b15080a5dbcd81084ddfea1fe045b9c968936f
Polina <polya_star95@mail.ru>
2018-02-16 12:56:37 +0300

    Added admin to the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lyvwf1pte+p(89f*41=qi_&i8hnlj+84b$h09*eh2+(alsr#z6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'Polina.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sumpol.pythonanywhere.com']
 
 
 # Application definition
@@ -120,3 +120,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
